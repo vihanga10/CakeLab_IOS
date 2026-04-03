a867a8cc93d7cd3684c2661056f41a8c0ecfdae1
Vihanga Madushamini <163097987+vihanga10@users.noreply.github.com>
2026-04-03 10:07:01 +0530

    test

---
+++ CakeLab_IOS/CakeLab_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeLab_IOSApp.swift
+//  CakeLab_IOS
+//
+//  Created by Vihanga Madushamini on 2026-04-03.
+//
+
+import SwiftUI
+
+@main
+struct CakeLab_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CakeLab_IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CakeLab_IOS
+//
+//  Created by Vihanga Madushamini on 2026-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
